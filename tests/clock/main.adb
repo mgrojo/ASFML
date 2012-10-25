e64eb8fd5226bdd1724f8bccd9577f7d1171509a
Dan Vazquez <danleedvm86@hotmail.com>
2012-10-25 21:17:13 +0200

    Updated test files

--- tests/clock/main.adb
+++ tests/clock/main.adb
@@ -1,21 +1,21 @@
-with Ada.Text_IO;		use Ada.Text_IO;
-with Ada.Float_Text_IO;	use Ada.Float_Text_IO;
-with Sf.System.Clock; 	use Sf.System.Clock;
-with Sf.System.Sleep;	use Sf.System.Sleep;
-with Sf.System.Types;	use Sf.System.Types;
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+with Sf.System.Clock;   use Sf.System.Clock;
+with Sf.System.Sleep;   use Sf.System.Sleep;
+with Sf.System.Types;   use Sf.System.Types;
 
 procedure Main is
 
-	My_Clock : sfClock_Ptr;
-	
+   My_Clock : sfClock_Ptr;
+
 begin
 
-	My_Clock := sfClock_Create;
-	sfSleep(0.05);
-	Put("Time elapsed(s): ");
-	Put(sfClock_GetTime(My_Clock), 0, 3, 0);
-	New_Line;
-	sfClock_Reset(My_Clock);
-	sfClock_Destroy(My_Clock);
+   My_Clock := sfClock_Create;
+   sfSleep (0.05);
+   Put ("Time elapsed(s): ");
+   Put (sfClock_GetTime (My_Clock), 0, 3, 0);
+   New_Line;
+   sfClock_Reset (My_Clock);
+   sfClock_Destroy (My_Clock);
 
 end Main;
--- tests/thread/main.adb
+++ tests/thread/main.adb
@@ -1,32 +1,32 @@
-with Ada.Text_IO;			use Ada.Text_IO;
-with Sf.Config;				use Sf.Config;
-with Sf.System.Thread; 		use Sf.System.Thread;
-with Sf.System.Sleep;		use Sf.System.Sleep;
-with Sf.System.Types;		use Sf.System.Types;
+with Ada.Text_IO;      use Ada.Text_IO;
+with Sf.Config;        use Sf.Config;
+with Sf.System.Thread; use Sf.System.Thread;
+with Sf.System.Sleep;  use Sf.System.Sleep;
+with Sf.System.Types;  use Sf.System.Types;
 
 procedure Main is
-	
-	procedure Thread_Func(Arg : sfVoid_Ptr) is
-	begin
-		for I in 1..10 loop
-			Put_Line("I'm thread 1");
-		    sfSleep(0.001);
-		end loop;
-	end Thread_Func;
-	
-	Thread : sfThread_Ptr;
-	UData  : sfVoid_Ptr;
-	
+
+   procedure Thread_Func (Arg : sfVoid_Ptr) is
+   begin
+      for I in 1 .. 10 loop
+         Put_Line ("I'm thread 1");
+         sfSleep (0.001);
+      end loop;
+   end Thread_Func;
+
+   Thread : sfThread_Ptr;
+   UData  : sfVoid_Ptr;
+
 begin
 
-	Thread := sfThread_Create(Thread_Func'Access, Udata);
-	sfThread_Launch(Thread);
-	
-	for I in 1..10 loop
-		Put_Line("I'm main thread");
-		sfSleep(0.001);
-	end loop;
+   Thread := sfThread_Create (Thread_Func'ACCESS, UData);
+   sfThread_Launch (Thread);
 
-	sfThread_Destroy(Thread);
-	
+   for I in 1 .. 10 loop
+      Put_Line ("I'm main thread");
+      sfSleep (0.001);
+   end loop;
+
+   sfThread_Destroy (Thread);
+
 end Main;
