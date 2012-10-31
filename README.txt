10cef0ba615ec6d78c0e40c5c7ceb67ce79cbdd5
Dan Vazquez <danleedvm86@hotmail.com>
2012-10-31 18:38:47 +0100

    Updated

--- README.txt
+++ README.txt
@@ -1,2 +1,28 @@
-ASFML-1.6 is a Ada95 and above binding to the SFML-1.6 library. It uses ada types
-and portable defined types which eliminates the use of interfaces.c libraries.
+###############
+# Description #
+###############
+
+ASFML-1.6 is a Ada95 binding to the SFML-1.6 library. It uses ada types
+and portable defined types which eliminates the inclusion of Ada interface
+libraries.
+
+The library has been tested with the following compilers: MinGW 4.6.2 and GNAT GPL 2012.
+It is writen in standard Ada95 without the use of any specific GNAT extensions.
+
+##################
+# How to build   #
+##################
+
+Open a terminal and go to "include" in your ASFML-1.6 directory
+gnatmake *.adb
+gnatmake *.ads
+
+NOTE: This step is not required but prevents the recompilation of the library every
+time you use it.
+
+##################
+# How to use     #
+##################
+
+To compile your program you need to use:
+gnatmake <filename>.adb -I<ASFML-1.6/include> -largs -L<ASFML-1.6/lib> -l<csfml-libs> -l<sfml-libs> -lstdc++
---
+++ main.adb
@@ -0,0 +1,75 @@
+-- This file is used to illustrate all
+-- the files in the library
+
+-- Root package
+with Sf;
+
+-- Used in all modules
+with Sf.Config;
+
+-- System module
+with Sf.System;
+with Sf.System.Types;
+with Sf.System.Clock;
+with Sf.System.Mutex;
+with Sf.System.Randomizer;
+with Sf.System.Sleep;
+with Sf.System.Thread;
+
+-- Window module
+with Sf.Window;
+with Sf.Window.Types;
+with Sf.Window.Context;
+with Sf.Window.Event;
+with Sf.Window.Input;
+with Sf.Window.VideoMode;
+with Sf.Window.Window;
+with Sf.Window.WindowHandle;
+
+-- Graphics module
+with Sf.Graphics;
+with Sf.Graphics.Types;
+with Sf.Graphics.BlendMode;
+with Sf.Graphics.Color;
+with Sf.Graphics.Font;
+with Sf.Graphics.Glyph;
+with Sf.Graphics.Image;
+with Sf.Graphics.PostFX;
+with Sf.Graphics.Rect;
+with Sf.Graphics.RenderWindow;
+with Sf.Graphics.Shape;
+with Sf.Graphics.Sprite;
+with Sf.Graphics.String;
+with Sf.Graphics.View;
+
+-- Audio module
+with Sf.Audio;
+with Sf.Audio.Types;
+with Sf.Audio.Listener;
+with Sf.Audio.Music;
+with Sf.Audio.Sound;
+with Sf.Audio.SoundBuffer;
+with Sf.Audio.SoundBufferRecorder;
+with Sf.Audio.SoundRecorder;
+with Sf.Audio.SoundStatus;
+with Sf.Audio.SoundStream;
+
+-- Network module
+with Sf.Network;
+with Sf.Network.Types;
+with Sf.Network.Ftp;
+with Sf.Network.Http;
+with Sf.Network.IPAddress;
+with Sf.Network.Packet;
+with Sf.Network.Selector;
+with Sf.Network.SocketStatus;
+with Sf.Network.SocketTCP;
+with Sf.Network.SocketUDP;
+
+procedure Main is
+
+begin
+
+   null;
+
+end Main;
