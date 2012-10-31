d86a966a4a391ddf92661e01ee394e5170df2a53
Dan Vazquez <danleedvm86@hotmail.com>
2012-10-31 15:56:25 +0100

    Deleted

--- tests/thread/threadfuncs.ads
+++
@@ -1,9 +0,0 @@
-with Sf.Config;       use Sf.Config;
-with Sf.System.Sleep; use Sf.System.Sleep;
-with Ada.Text_IO;     use Ada.Text_IO;
-
-package ThreadFuncs is
-
-   procedure Thread_Func (Arg : sfVoid_Ptr);
-
-end ThreadFuncs;
