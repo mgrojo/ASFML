7acd625489234ffa1c76644d42935e87f5f73881
Dan Vazquez <danleedvm86@hotmail.com>
2012-10-31 15:56:34 +0100

    Updated

--- tests/thread/main.adb
+++ tests/thread/main.adb
@@ -3,12 +3,19 @@ with Sf.Config;        use Sf.Config;
 with Sf.System.Thread; use Sf.System.Thread;
 with Sf.System.Sleep;  use Sf.System.Sleep;
 with Sf.System.Types;  use Sf.System.Types;
-with ThreadFuncs;
 
 procedure Main is
 
+   procedure Thread_Func (Arg : sfVoid_Ptr) is
+   begin
+      for I in 1 .. 10 loop
+         Put_Line ("I'm thread 1");
+         sfSleep (0.001);
+      end loop;
+   end Thread_Func;
+
    Thread : sfThread_Ptr;
-   TFunc  : sfThreadFunc_Ptr := ThreadFuncs.Thread_Func'ACCESS;
+   TFunc  : sfThreadFunc_Ptr := Thread_Func'UNRESTRICTED_ACCESS;
    UData  : sfVoid_Ptr;
 
 begin
