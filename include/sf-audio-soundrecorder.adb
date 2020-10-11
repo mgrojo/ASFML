e20383ca35bb050c9cda43d4c6ed93fe55c08159
mgr <mgrojo@gmail.com>
2020-10-11 15:39:08 +0200

    Fix: Porting to CSFML 2.4 complete
    Fix commit 517d9b38cb9b8f83239a6b5c22af3b84cdd2421d

---
+++ include/sf-audio-soundrecorder.adb
@@ -0,0 +1,95 @@
+with Interfaces.C.Strings; use Interfaces.C;
+
+with System;
+
+with Ada.Strings.Unbounded;
+
+package body Sf.Audio.SoundRecorder is
+
+
+  --//////////////////////////////////////////////////////////
+  --/ \brief Get a list of the names of all availabe audio capture devices
+  --/
+  --/ This function returns an array of strings (null terminated),
+  --/ containing the names of all availabe audio capture devices.
+  --/ If no devices are available then NULL is returned.
+  --/
+  --/ \param count Pointer to a variable that will be filled with the number of modes in the array
+  --/
+  --/ \return An array of strings containing the names
+  --/
+  --//////////////////////////////////////////////////////////
+  function sfSoundRecorder_getAvailableDevices return sfArrayOfStrings is
+
+     function Internal (count : access sfSize_t) return Standard.System.Address;
+     pragma Import (C, Internal, "sfSoundRecorder_getAvailableDevices");
+
+     count : aliased sfSize_t;
+     result : Standard.System.Address := Internal (count'access);
+
+   begin
+      declare
+         subtype availableDevicesType is Strings.Chars_Ptr_Array (0 .. size_t (count) - 1);
+         cAvailableDevices : availableDevicesType;
+         pragma Import (Ada, cAvailableDevices);
+         for cAvailableDevices'Address use result;
+
+         AdaAvailableDevices : sfArrayOfStrings (0 .. count - 1);
+
+      begin
+         for i in availableDevicesType'Range loop
+            AdaAvailableDevices(sfSize_t(i)) := Ada.Strings.Unbounded.To_Unbounded_String
+               (Interfaces.C.Strings.Value (cAvailableDevices(i)));
+         end loop;
+
+         return AdaAvailableDevices;
+      end;
+   end sfSoundRecorder_getAvailableDevices;
+
+  --//////////////////////////////////////////////////////////
+  --/ \brief Set the audio capture device
+  --/
+  --/ This function sets the audio capture device to the device
+  --/ with the given name. It can be called on the fly (i.e:
+  --/ while recording). If you do so while recording and
+  --/ opening the device fails, it stops the recording.
+  --/
+  --/ \param soundRecorder Sound recorder object
+  --/ \param name          The name of the audio capture device
+  --/
+  --/ \return sfTrue, if it was able to set the requested device
+  --/
+  --//////////////////////////////////////////////////////////
+  function sfSoundRecorder_setDevice
+    (soundRecorder : sfSoundRecorder_Ptr; name : String)
+    return sfBool is
+
+     function Internal
+        (soundRecorder : sfSoundRecorder_Ptr; name : Interfaces.C.Strings.chars_ptr)
+        return sfBool;
+      pragma Import (C, Internal, "sfSoundRecorder_setDevice");
+
+      Temp : Interfaces.C.Strings.chars_ptr := Interfaces.C.Strings.New_String (name);
+      Result : sfBool := Internal (soundRecorder, Temp);
+   begin
+      Interfaces.C.Strings.Free (Temp);
+      return Result;
+  end sfSoundRecorder_setDevice;
+
+  --//////////////////////////////////////////////////////////
+  --/ \brief Get the name of the current audio capture device
+  --/
+  --/ \param soundRecorder Sound recorder object
+  --/
+  --/ \return The name of the current audio capture device
+  --/
+  --//////////////////////////////////////////////////////////
+  function sfSoundRecorder_getDevice (soundRecorder : sfSoundRecorder_Ptr) return String is
+     function Internal (soundRecorder : sfSoundRecorder_Ptr) return Interfaces.C.Strings.chars_ptr;
+     pragma Import (C, Internal, "sfSoundRecorder_getDevice");
+  begin
+
+     return Interfaces.C.Strings.Value (Internal (soundRecorder));
+  end sfSoundRecorder_getDevice;
+
+end Sf.Audio.SoundRecorder;
---
+++ include/sf-network-tcplistener.ads
@@ -0,0 +1,135 @@
+--//////////////////////////////////////////////////////////
+-- SFML - Simple and Fast Multimedia Library
+-- Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+-- This software is provided 'as-is', without any express or implied warranty.
+-- In no event will the authors be held liable for any damages arising from the use of this software.
+-- Permission is granted to anyone to use this software for any purpose,
+-- including commercial applications, and to alter it and redistribute it freely,
+-- subject to the following restrictions:
+-- 1. The origin of this software must not be misrepresented;
+--    you must not claim that you wrote the original software.
+--    If you use this software in a product, an acknowledgment
+--    in the product documentation would be appreciated but is not required.
+-- 2. Altered source versions must be plainly marked as such,
+--    and must not be misrepresented as being the original software.
+-- 3. This notice may not be removed or altered from any source distribution.
+--//////////////////////////////////////////////////////////
+
+with Interfaces.C; use Interfaces.C;
+with System;
+with Sf.Config;
+with Sf.Network.IpAddress;
+with Sf.Network.SocketStatus;
+with Sf.Network.Types;
+
+package Sf.Network.TcpListener is
+   use Types;
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Create a new TCP listener
+   --/
+   --/ \return A new sfTcpListener object
+   --/
+   --//////////////////////////////////////////////////////////
+   function sfTcpListener_create return sfTcpListener_Ptr;
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Destroy a TCP listener
+   --/
+   --/ \param listener TCP listener to destroy
+   --/
+   --//////////////////////////////////////////////////////////
+   procedure sfTcpListener_destroy (listener : sfTcpListener_Ptr);
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Set the blocking state of a TCP listener
+   --/
+   --/ In blocking mode, calls will not return until they have
+   --/ completed their task. For example, a call to
+   --/ sfTcpListener_accept in blocking mode won't return until
+   --/ a new connection was actually received.
+   --/ In non-blocking mode, calls will always return immediately,
+   --/ using the return code to signal whether there was data
+   --/ available or not.
+   --/ By default, all sockets are blocking.
+   --/
+   --/ \param listener TCP listener object
+   --/ \param blocking sfTrue to set the socket as blocking, sfFalse for non-blocking
+   --/
+   --//////////////////////////////////////////////////////////
+   procedure sfTcpListener_setBlocking (listener : sfTcpListener_Ptr; blocking : Sf.Config.sfBool);
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Tell whether a TCP listener is in blocking or non-blocking mode
+   --/
+   --/ \param listener TCP listener object
+   --/
+   --/ \return sfTrue if the socket is blocking, sfFalse otherwise
+   --/
+   --//////////////////////////////////////////////////////////
+   function sfTcpListener_isBlocking (listener : sfTcpListener_Ptr) return Sf.Config.sfBool;
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Get the port to which a TCP listener is bound locally
+   --/
+   --/ If the socket is not listening to a port, this function
+   --/ returns 0.
+   --/
+   --/ \param listener TCP listener object
+   --/
+   --/ \return Port to which the TCP listener is bound
+   --/
+   --//////////////////////////////////////////////////////////
+   function sfTcpListener_getLocalPort (listener : sfTcpListener_Ptr) return unsigned_short;
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Start listening for connections
+   --/
+   --/ This functions makes the socket listen to the specified
+   --/ port, waiting for new connections.
+   --/ If the socket was previously listening to another port,
+   --/ it will be stopped first and bound to the new port.
+   --/
+   --/ If there is no specific address to listen to, pass sfIpAddress_Any
+   --/
+   --/ \param listener TCP listener object
+   --/ \param port     Port to listen for new connections
+   --/ \param address  Address of the interface to listen on
+   --/
+   --/ \return Status code
+   --/
+   --//////////////////////////////////////////////////////////
+   function sfTcpListener_listen
+     (listener : sfTcpListener_Ptr;
+      port : unsigned_short;
+      address : Sf.Network.IpAddress.sfIpAddress) return Sf.Network.SocketStatus.sfSocketStatus;
+
+   --//////////////////////////////////////////////////////////
+   --/ \brief Accept a new connection
+   --/
+   --/ If the socket is in blocking mode, this function will
+   --/ not return until a connection is actually received.
+   --/
+   --/ The \a connected argument points to a valid sfTcpSocket pointer
+   --/ in case of success (the function returns sfSocketDone), it points
+   --/ to a NULL pointer otherwise.
+   --/
+   --/ \param listener  TCP listener object
+   --/ \param connected Socket that will hold the new connection
+   --/
+   --/ \return Status code
+   --/
+   --//////////////////////////////////////////////////////////
+   function sfTcpListener_accept (listener : sfTcpListener_Ptr; connected : in out sfTcpSocket_Ptr) return Sf.Network.SocketStatus.sfSocketStatus;
+
+private
+
+   pragma Import (C, sfTcpListener_create, "sfTcpListener_create");
+   pragma Import (C, sfTcpListener_destroy, "sfTcpListener_destroy");
+   pragma Import (C, sfTcpListener_setBlocking, "sfTcpListener_setBlocking");
+   pragma Import (C, sfTcpListener_isBlocking, "sfTcpListener_isBlocking");
+   pragma Import (C, sfTcpListener_getLocalPort, "sfTcpListener_getLocalPort");
+   pragma Import (C, sfTcpListener_listen, "sfTcpListener_listen");
+   pragma Import (C, sfTcpListener_accept, "sfTcpListener_accept");
+
+end Sf.Network.TcpListener;
