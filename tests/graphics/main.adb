mgrojo/ASFML

commit b4b35b941ede652c088db98e41c844b2ad14e639

Removing some C-isms in identifiers from the binding
Since the package gives the context in Ada and provides a specific
namespace, the prefixes are unnecessary and redundant.

In some cases, Ada 2012 aspects syntax has been used to allow an overloaded
name to be imported.

include/sf-graphics-rect.ads
@@ -51,15 +51,17 @@ package Sf.Graphics.Rect is
    --/ @return sfTrue if the point is inside
    --/
    --//////////////////////////////////////////////////////////
-   function sfFloatRect_contains
-     (rect : access constant sfFloatRect;
+   function contains
+     (rect : sfFloatRect;
       x : Float;
-      y : Float) return sfBool;
+      y : Float) return sfBool
+     with Import, Convention => C, External_Name => "sfFloatRect_contains";
 
-   function sfIntRect_contains
-     (rect : access constant sfIntRect;
+   function contains
+     (rect : sfIntRect;
       x : Integer;
-      y : Integer) return sfBool;
+      y : Integer) return sfBool
+     with Import, Convention => C, External_Name => "sfIntRect_contains";
 
    --//////////////////////////////////////////////////////////
    --/ @brief Check intersection between two rectangles
@@ -71,23 +73,23 @@ package Sf.Graphics.Rect is
    --/ @return sfTrue if rectangles overlap
    --/
    --//////////////////////////////////////////////////////////
-   function sfFloatRect_intersects
+   function intersects
      (rect1 : access constant sfFloatRect;
       rect2 : access constant sfFloatRect;
-      intersection : access sfFloatRect) return sfBool;
+      intersection : access sfFloatRect) return sfBool
+     with Import, Convention => C, External_Name => "sfFloatRect_intersects";
 
-   function sfIntRect_intersects
+   function intersects
      (rect1 : access constant sfIntRect;
       rect2 : access constant sfIntRect;
-      intersection : access sfIntRect) return sfBool;
+      intersection : access sfIntRect) return sfBool
+     with Import, Convention => C, External_Name => "sfIntRect_intersects";
 
 private
+   
+   pragma Convention (C_Pass_By_Copy, sfFloatRect);
+   pragma Convention (C_Pass_By_Copy, sfIntRect);
 
-   pragma Import (C, sfFloatRect_contains, "sfFloatRect_contains");
-   pragma Import (C, sfIntRect_contains, "sfIntRect_contains");
-   pragma Import (C, sfFloatRect_intersects, "sfFloatRect_intersects");
-   pragma Import (C, sfIntRect_intersects, "sfIntRect_intersects");
    sfNullRectangle : aliased constant sfIntRect := (0, 0, 0, 0);
-
 
 end Sf.Graphics.Rect;

tests/clock/main.adb
@@ -12,18 +12,18 @@ procedure Main is
 
 begin
 
-   My_Clock := sfClock_Create;
+   My_Clock := Create;
    sfDelay(0.05);
    Put ("Time elapsed(s): ");
-   Put (sfTime_asSeconds (sfClock_GetElapsedTime (My_Clock)), Fore => 0, Aft => 3, Exp => 0);
+   Put (asSeconds (GetElapsedTime (My_Clock)), Fore => 0, Aft => 3, Exp => 0);
    New_Line;
    Put ("Time elapsed(ms) since start: ");
-   Put (Integer (sfTime_asMilliseconds (sfClock_Restart (My_Clock))));
+   Put (Integer (asMilliseconds (Restart (My_Clock))));
    New_Line;
    sfSleep(sfMilliseconds(1050));
    Put ("Time elapsed(ms): ");
-   Put (Integer (sfTime_asMilliseconds (sfClock_GetElapsedTime (My_Clock))));
+   Put (Integer (asMilliseconds (GetElapsedTime (My_Clock))));
    New_Line;
-   sfClock_Destroy (My_Clock);
+   Destroy (My_Clock);
 
 end Main;

tests/example/main.adb
@@ -27,65 +27,65 @@ procedure Main is
    Font : sfFont_Ptr;
    Text : sfText_Ptr;
    Music : sfMusic_Ptr;
-   event : aliased sfEvent;
+   event : sfEvent;
 
 begin
 
    -- Create the main Window
-   Window := sfRenderWindow_create(Mode, "SFML window", sfResize or sfClose, sfDefaultContextSettings);
+   Window := create(Mode, "SFML window", sfResize or sfClose, sfDefaultContextSettings);
 
    -- Load a sprite to display
-   Texture := sfTexture_createFromFile("cute_image.jpg", null);
+   Texture := createFromFile("cute_image.jpg", null);
 
-   Sprite := sfSprite_create;
-   sfSprite_setTexture(Sprite, Texture, sfTrue);
+   Sprite := create;
+   setTexture(Sprite, Texture, sfTrue);
 
    -- Create a graphical text to display
-   Font := sfFont_createFromFile("arial.ttf");
+   Font := createFromFile("arial.ttf");
 
-   Text := sfText_create;
-   sfText_setString(Text, "Hello SFML");
-   sfText_setFont(Text, Font);
-   sfText_setCharacterSize(Text, 50);
+   Text := create;
+   setString(Text, "Hello SFML");
+   setFont(Text, Font);
+   setCharacterSize(Text, 50);
 
    -- Load a music file to play
-   Music := sfMusic_createFromFile("nice_music.ogg");
+   Music := createFromFile("nice_music.ogg");
 
    -- Play the music
-   sfMusic_play(Music);
+   play(Music);
 
    -- Start the game loop
-   while sfRenderWindow_isOpen(Window) loop
+   while isOpen(Window) loop
 
       -- Process events
-      while sfRenderWindow_pollEvent(Window, event'access) loop
+      while pollEvent(Window, event) loop
 
          -- Close window : exit
          if (event.eventType = sfEvtClosed) then
-            sfRenderWindow_close(Window);
+            close(Window);
          end if;
       end loop;
 
       -- Clear the screen
-      sfRenderWindow_clear(Window, sfBlack);
+      clear(Window, sfBlack);
 
       -- Draw the sprite
-      sfRenderWindow_drawSprite(Window, Sprite, null);
+      drawSprite(Window, Sprite, null);
 
       -- Draw the text
-      sfRenderWindow_drawText(Window, Text, null);
+      drawText(Window, Text, null);
 
       -- Update the window
-      sfRenderWindow_display(Window);
+      display(Window);
    end loop;
 
    -- Cleanup resources
-   sfMusic_destroy(Music);
-   sfText_destroy(Text);
-   sfFont_destroy(Font);
-   sfSprite_destroy(Sprite);
-   sfTexture_destroy(Texture);
-   sfRenderWindow_destroy(Window);
+   destroy(Music);
+   destroy(Text);
+   destroy(Font);
+   destroy(Sprite);
+   destroy(Texture);
+   destroy(Window);
 
 exception
    when others =>

tests/music/main.adb
@@ -19,22 +19,22 @@ procedure Main is
 
 begin
 
-   Music := sfMusic_createFromFile (Ada.Command_Line.Argument (1));
+   Music := createFromFile (Ada.Command_Line.Argument (1));
    if Music = null then
       Put_Line ("Music file not found!");
       return;
    end if;
 
-   sfMusic_setLoop (Music, sfFalse);
-   sfMusic_setPitch (Music, 1.0);
-   sfMusic_setVolume (Music, 100.0);
+   setLoop (Music, sfFalse);
+   setPitch (Music, 1.0);
+   setVolume (Music, 100.0);
 
-   Duration := sfMusic_getDuration (Music);
-   ChCount  := sfMusic_getChannelCount (Music);
-   SampRate := sfMusic_getSampleRate (Music);
+   Duration := getDuration (Music);
+   ChCount  := getChannelCount (Music);
+   SampRate := getSampleRate (Music);
 
    Put ("Duration       : ");
-   Put (Integer (sfTime_asMilliseconds (Duration)));
+   Put (Integer (asMilliseconds (Duration)));
    Put (" milliseconds");
    New_Line;
    Put ("Channels count : ");
@@ -46,18 +46,18 @@ begin
    Put (" channels/second");
    New_Line;
 
-   sfMusic_play (Music);
+   play (Music);
 
    Put ("Playing... ");
-   while sfMusic_getStatus (Music) = sfPlaying loop
+   while getStatus (Music) = sfPlaying loop
 
       -- Display the playing position
-      Put (Integer (sfTime_asMilliseconds (sfMusic_getPlayingOffset(Music))));
+      Put (Integer (asMilliseconds (getPlayingOffset(Music))));
       Put (" ");
       delay 0.1;
 
    end loop;
 
-   sfMusic_destroy (Music);
+   destroy (Music);
 
 end Main;

tests/opengl/main.adb
@@ -20,16 +20,16 @@ procedure Main is
 
 begin
 
-   Clock := sfClock_Create;
+   Clock := Create;
 
-   Window := sfWindow_Create (Mode, "Window", sfClose, Params);
+   Window := Create (Mode, "Window", sfClose, Params);
    if Window = null then
       Put_Line ("Failed to create window");
       return;
    end if;
-   sfWindow_SetFramerateLimit (Window, 32);
-   sfWindow_setVerticalSyncEnabled (Window, sfFalse);
-   sfWindow_SetVisible (Window, sfTrue);
+   SetFramerateLimit (Window, 32);
+   setVerticalSyncEnabled (Window, sfFalse);
+   SetVisible (Window, sfTrue);
 
    --opengl stuff
    glClearDepth (1.0);
@@ -41,13 +41,13 @@ begin
    glLoadIdentity;
    gluPerspective (90.0, 1.0, 1.0, 500.0);
 
-   while sfWindow_IsOpen (Window) = sfTrue loop
-      while sfWindow_PollEvent (Window, Event'ACCESS) = sfTrue loop
+   while IsOpen (Window) = sfTrue loop
+      while PollEvent (Window, Event'ACCESS) = sfTrue loop
          if Event.eventType = sfEvtClosed then
-            sfWindow_Close (Window);
+            Close (Window);
             Put_Line ("Attepting to close");
-         elsif Event.eventType = sfEvtKeyPressed and then sfKeyboard_isKeyPressed (sfKeyEscape) = sfTrue then
-            sfWindow_Close (Window);
+         elsif Event.eventType = sfEvtKeyPressed and then isKeyPressed (sfKeyEscape) = sfTrue then
+            Close (Window);
             Put_Line ("Attepting to close");
          elsif Event.eventType = sfEvtResized then
             glViewport (0, 0, GLsizei (Event.Size.Width), GLsizei (Event.Size.Height));
@@ -61,9 +61,9 @@ begin
       glMatrixMode (GL_MODELVIEW);
       glLoadIdentity;
       glTranslatef (0.0, 0.0, -200.0);
-      glRotatef (GLfloat (sfTime_asSeconds (sfClock_getElapsedTime (Clock)) * 50.0), 1.0, 0.0, 0.0);
-      glRotatef (GLfloat (sfTime_asSeconds (sfClock_getElapsedTime (Clock)) * 30.0), 0.0, 1.0, 0.0);
-      glRotatef (GLfloat (sfTime_asSeconds (sfClock_getElapsedTime (Clock)) * 90.0), 0.0, 0.0, 1.0);
+      glRotatef (GLfloat (asSeconds (getElapsedTime (Clock)) * 50.0), 1.0, 0.0, 0.0);
+      glRotatef (GLfloat (asSeconds (getElapsedTime (Clock)) * 30.0), 0.0, 1.0, 0.0);
+      glRotatef (GLfloat (asSeconds (getElapsedTime (Clock)) * 90.0), 0.0, 0.0, 1.0);
       glBegin (GL_QUADS);
       glVertex3f (-50.0, -50.0, -50.0);
       glVertex3f (-50.0, 50.0, -50.0);
@@ -91,10 +91,10 @@ begin
       glVertex3f (50.0, 50.0, 50.0);
       glEnd;
 
-      sfWindow_Display (Window);
+      Display (Window);
       sfDelay (0.001);
    end loop;
-   sfWindow_Destroy (Window);
-   sfClock_Destroy (Clock);
+   Destroy (Window);
+   Destroy (Clock);
 
 end Main;

tests/renderwindow/main.adb
@@ -21,7 +21,7 @@ procedure Main is
    Window : sfRenderWindow_Ptr;
    Mode   : sfVideoMode      := (640, 480, 32);
    Params : sfContextSettings := sfDefaultContextSettings;
-   Event  : aliased sfEvent;
+   Event  : sfEvent;
 
    Sprite : sfSprite_Ptr;
    Img    : sfTexture_Ptr;
@@ -31,92 +31,92 @@ procedure Main is
 
 begin
 
-   Img := sfTexture_CreateFromFile ("logo.png");
+   Img := CreateFromFile ("logo.png");
    if Img = null then
       Put_Line ("Could not open image");
       return;
    end if;
 
-   Icon := sfImage_CreateFromFile ("sfml-icon.png");
+   Icon := CreateFromFile ("sfml-icon.png");
    if Icon = null then
       Put_Line ("Could not open icon");
-      sfTexture_Destroy (Img);
+      Destroy (Img);
       return;
    end if;
 
-   Sprite := sfSprite_Create;
+   Sprite := Create;
    if Sprite = null then
       Put_Line ("Could not create sprite");
-      sfTexture_Destroy (Img);
+      Destroy (Img);
       return;
    end if;
-   sfSprite_SetTexture (Sprite, Img);
-   sfSprite_SetPosition (Sprite,
-                         (x => Float (sfUint32 (Mode.Width) / 2 - sfTexture_GetSize (Img).x / 2),
-                          y => Float (sfUint32 (Mode.Height) / 2 - sfTexture_GetSize (Img).y / 2)));
+   SetTexture (Sprite, Img);
+   SetPosition (Sprite,
+                         (x => Float (sfUint32 (Mode.Width) / 2 - GetSize (Img).x / 2),
+                          y => Float (sfUint32 (Mode.Height) / 2 - GetSize (Img).y / 2)));
    --sfSprite_SetBlendMode (Sprite, sfBlendAlpha);
 
-   Font := sfFont_CreateFromFile("aerial.ttf");
+   Font := CreateFromFile("aerial.ttf");
    if Font = null then
       Put_Line ("Could not get font");
-      sfSprite_Destroy (Sprite);
-      sfTexture_Destroy (Img);
+      Destroy (Sprite);
+      Destroy (Img);
       return;
    end if;
 
-   Str := sfText_Create;
+   Str := Create;
    if Str = null then
       Put_Line ("Could not create string");
-      sfSprite_Destroy (Sprite);
-      sfTexture_Destroy (Img);
-      sfFont_Destroy(Font);
+      Destroy (Sprite);
+      Destroy (Img);
+      Destroy(Font);
       return;
    end if;
-   sfText_SetFont (Str, Font);
-   sfText_SetString (Str, "The SFML Logo" & Character'Val (10) & "In Aerial Font");
+   SetFont (Str, Font);
+   SetString (Str, "The SFML Logo" & Character'Val (10) & "In Aerial Font");
    --sfText_SetSize(Str, 20.0);
-   sfText_SetPosition (Str, (Float (Mode.Width / 2) - (sfText_GetGlobalBounds (Str).Width) / 2.0,
+   SetPosition (Str, (Float (Mode.Width / 2) - (GetGlobalBounds (Str).Width) / 2.0,
                              Float (Mode.Height / 2) + 60.0));
-   sfText_SetColor (Str, sfBlue);
+   SetColor (Str, sfBlue);
 
-   Window := sfRenderWindow_Create (Mode, "Ada SFML Window", sfResize or sfClose, Params);
+   Window := Create (Mode, "Ada SFML Window", sfResize or sfClose, Params);
    if Window = null then
       Put_Line ("Failed to create window");
       return;
    end if;
-   sfRenderWindow_SetFramerateLimit (Window, 32);
-   sfRenderWindow_SetVerticalSyncEnabled (Window, sfFalse);
-   sfRenderWindow_SetVisible (Window, sfTrue);
+   SetFramerateLimit (Window, 32);
+   SetVerticalSyncEnabled (Window, sfFalse);
+   SetVisible (Window, sfTrue);
 
-   sfRenderWindow_SetIcon (Window, sfImage_GetSize (Icon).x, sfImage_GetSize (Icon).y,
-                           sfImage_GetPixelsPtr (Icon));
+   SetIcon (Window, GetSize (Icon).x, GetSize (Icon).y,
+                           GetPixelsPtr (Icon));
 
-   while sfRenderWindow_IsOpen (Window) = sfTrue loop
-      while sfRenderWindow_PollEvent (Window, Event'Access) = sfTrue loop
+   while IsOpen (Window) = sfTrue loop
+      while PollEvent (Window, Event) = sfTrue loop
          if Event.eventType = sfEvtClosed then
-            sfRenderWindow_Close (Window);
+            Close (Window);
             Put_Line ("Attempting to close");
          end if;
          if Event.eventType = sfEvtKeyPressed
            and then Event.key.code = sfKeyEscape then
-            sfRenderWindow_Close (Window);
+            Close (Window);
             Put_Line ("Attempting to close");
          end if;
       end loop;
-      sfRenderWindow_Clear (Window, sfWhite);
+      Clear (Window, sfWhite);
 
-      sfRenderWindow_DrawSprite (Window, Sprite);
-      sfRenderWindow_DrawText (Window, Str);
+      DrawSprite (Window, Sprite);
+      DrawText (Window, Str);
 
-      sfRenderWindow_Display (Window);
+      Display (Window);
       sfSleep (sfSeconds (0.001));
    end loop;
 
-   sfRenderWindow_Destroy (Window);
-   sfSprite_Destroy (Sprite);
-   sfTexture_Destroy (Img);
-   sfImage_Destroy (Icon);
-   sfText_Destroy (Str);
-   sfFont_Destroy(Font);
+   Destroy (Window);
+   Destroy (Sprite);
+   Destroy (Img);
+   Destroy (Icon);
+   Destroy (Str);
+   Destroy(Font);
 
 end Main;

tests/thread/main.adb
@@ -13,14 +13,14 @@ procedure Main is
 
 begin
 
-   Thread := sfThread_Create (TFunc, UData'Address);
-   sfThread_Launch (Thread);
+   Thread := Create (TFunc, UData'Address);
+   Launch (Thread);
 
    for I in 1 .. 10 loop
       Put_Line ("I'm main thread");
       sfDelay (0.001);
    end loop;
 
-   sfThread_Destroy (Thread);
+   Destroy (Thread);
 
 end Main;

tests/window/main.adb
@@ -14,29 +14,29 @@ procedure Main is
 
 begin
 
-   Window := sfWindow_Create (Mode, "Window");
+   Window := Create (Mode, "Window");
    if Window = null then
       Put_Line ("Failed to create window");
       return;
    end if;
-   sfWindow_SetFramerateLimit (Window, 32);
-   sfWindow_SetVerticalSyncEnabled (Window, sfTrue);
+   SetFramerateLimit (Window, 32);
+   SetVerticalSyncEnabled (Window, sfTrue);
 
-   while sfWindow_IsOpen (Window) = sfTrue loop
-      while sfWindow_PollEvent (Window, Event'Access) = sfTrue loop
+   while IsOpen (Window) = sfTrue loop
+      while PollEvent (Window, Event'Access) = sfTrue loop
          if Event.eventType = sfEvtClosed then
-            sfWindow_Close (Window);
+            Close (Window);
             Put_Line ("Attempting to close");
          end if;
          if Event.eventType = sfEvtKeyPressed
            and then Event.key.code = sfKeyEscape then
-            sfWindow_Close (Window);
+            Close (Window);
             Put_Line ("Attempting to close");
          end if;
       end loop;
-      sfWindow_Display (Window);
+      Display (Window);
       sfSleep (sfSeconds (0.001));
    end loop;
-   sfWindow_Destroy (Window);
+   Destroy (Window);
 
 end Main;

commit 0b3d5a5a4a4c7d5bf8306bcf35a255ea45402c2b

Fix `Sf.Graphics.Rect.contains`
This problem was introduced in b4b35b941ede652c088db98e41c844b2ad14e639
when trying to simplify the API. It is possible to remove `access constant`
but if the Convention for the records is set to `C` instead of
`C_Pass_By_Copy`. In that case, the `intersects` functions should also be
changed, so to minimize the API changes, `access constant` for `contains`
has been restored. Moreover, other parts of the API follow this convention.

Added test for these functions.

Added this test and pong to the Makefile used for compiling all the tests.

See issue #11

include/sf-graphics-rect.ads
@@ -52,13 +52,13 @@ package Sf.Graphics.Rect is
    --/
    --//////////////////////////////////////////////////////////
    function contains
-     (rect : sfFloatRect;
+     (rect : access constant sfFloatRect;
       x : Float;
       y : Float) return sfBool
      with Import, Convention => C, External_Name => "sfFloatRect_contains";
 
    function contains
-     (rect : sfIntRect;
+     (rect : access constant sfIntRect;
       x : Integer;
       y : Integer) return sfBool
      with Import, Convention => C, External_Name => "sfIntRect_contains";

tests/Makefile
@@ -1,5 +1,6 @@
 all:
-	for dir in clock  example  music  opengl  renderwindow  thread  window ; \
+	for dir in \
+		clock  example graphics  music  opengl pong  renderwindow  thread  window ; \
 	do \
 		$(MAKE) -C $$dir ; \
 	done

tests/graphics/main.adb
@@ -1,0 +1,68 @@
+with Sf.Graphics.Rect;
+with Sf.System.Vector2;
+with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
+
+procedure Main is
+
+   FloatRectangle : aliased constant Sf.Graphics.Rect.sfFloatRect := (0.00, 0.00, 100.00, 100.00);
+   IntegerRectangle : aliased constant Sf.Graphics.Rect.sfIntRect := (0, 0, 100, 100);
+
+   FloatPoint : constant Sf.System.Vector2.sfVector2f := (10.00, 10.00);
+   IntegerPoint : constant Sf.System.Vector2.sfVector2i := (10, 10);
+
+   OutFloatPoint : constant Sf.System.Vector2.sfVector2f := (200.00, 10.00);
+   OutIntegerPoint : constant Sf.System.Vector2.sfVector2i := (200, 10);
+
+begin
+
+
+   case
+     Sf.Graphics.Rect.contains (rect => FloatRectangle'Access,
+                                x    => FloatPoint.x,
+                                y    => FloatPoint.y)
+   is
+      when Sf.sfTrue =>
+         Put_Line ("OK: Floatpoint is inside.");
+
+      when Sf.sfFalse =>
+         Put_Line ("Fail: Floatpoint is outside.");
+   end case;
+
+   case
+     Sf.Graphics.Rect.contains (rect => IntegerRectangle'Access,
+                                x    => Integer (IntegerPoint.x),
+                                y    => Integer (IntegerPoint.y))
+   is
+      when Sf.sfTrue =>
+         Put_Line ("OK: Integerpoint is inside.");
+
+      when Sf.sfFalse =>
+         Put_Line ("Fail: Integerpoint is outside.");
+   end case;
+
+
+   case
+     Sf.Graphics.Rect.contains (rect => FloatRectangle'Access,
+                                x    => OutFloatPoint.x,
+                                y    => OutFloatPoint.y)
+   is
+      when Sf.sfTrue =>
+         Put_Line ("Fail: OutFloatpoint is inside.");
+
+      when Sf.sfFalse =>
+         Put_Line ("OK: OutFloatpoint is outside.");
+   end case;
+
+   case
+     Sf.Graphics.Rect.contains (rect => IntegerRectangle'Access,
+                                x    => Integer (OutIntegerPoint.x),
+                                y    => Integer (OutIntegerPoint.y))
+   is
+      when Sf.sfTrue =>
+         Put_Line ("Fail: OutIntegerpoint is inside.");
+
+      when Sf.sfFalse =>
+         Put_Line ("OK: OutIntegerpoint is outside.");
+   end case;
+
+end Main;

tests/graphics/main.gpr
@@ -1,0 +1,20 @@
+with "../../asfml.gpr";
+
+project Main is
+
+   for Create_Missing_Dirs use "True";
+
+   for Source_Dirs use (".");
+   for Object_Dir use "obj";
+   for Main use ("main.adb");
+   for Exec_Dir use ".";
+
+   package Compiler is
+
+      -- This flag is for strings to be interpreted in UTF-8 by
+      -- compiler.
+      for Default_Switches ("Ada") use ("-gnatW8");
+
+   end Compiler;
+
+end Main;

tests/graphics/makefile
@@ -1,0 +1,5 @@
+all:
+	gprbuild
+
+clean:
+	gprclean

tests/pong/makefile
@@ -1,0 +1,5 @@
+all:
+	gprbuild
+
+clean:
+	gprclean

tests/pong/pong.adb
@@ -150,7 +150,8 @@ begin
             end if;
          end if;
          if (CircleShape.getPosition (Ball).X - BallRadius < RectangleShape.getPosition (LeftPaddle).X + 0.5 * PaddleSize.X)
-           and (CircleShape.getPosition (Ball).X - BallRadius > RectangleShape.getPosition (LeftPaddle).X) and (CircleShape.getPosition (Ball).Y + BallRadius >= RectangleShape.getPosition (LeftPaddle).Y - 0.5 * PaddleSize.Y)
+           and (CircleShape.getPosition (Ball).X - BallRadius > RectangleShape.getPosition (LeftPaddle).X)
+           and (CircleShape.getPosition (Ball).Y + BallRadius >= RectangleShape.getPosition (LeftPaddle).Y - 0.5 * PaddleSize.Y)
            and (CircleShape.getPosition (Ball).Y - BallRadius <= RectangleShape.getPosition (LeftPaddle).Y + 0.5 * PaddleSize.Y)
          then
             if CircleShape.getPosition (Ball).Y > RectangleShape.getPosition (LeftPaddle).Y then