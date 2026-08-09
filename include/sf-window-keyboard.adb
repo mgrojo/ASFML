bdfb196f132363c51262ea10663ebe2a4c82ea69
mgrojo <mgrojo@gmail.com>
2026-08-10 00:27:18 +0200

    Avoid warning about no effect of use clause

--- include/sf-window-keyboard.adb
+++ include/sf-window-keyboard.adb
@@ -3,7 +3,7 @@ with Interfaces.C.Strings;
 package body Sf.Window.Keyboard is
    
    function getDescription (code : sfScancode) return String is
-      use Interfaces.C; use Interfaces.C.Strings;
+      use Interfaces.C.Strings;
       function Internal (code : sfScancode) return chars_ptr;
       pragma Import (C, Internal, "sfKeyboard_getDescription");
       C_Result : chars_ptr := Internal (code);
